75a344f10daaae6f13bb4e523afbd35f9ec0094a
Korawich Anuttra <korawich.anu@gmail.com>
2024-08-27 17:54:15 +0700

    :dart: feat: add static and midleware in fastapi application.

--- src/ddeutil/observe/app.py
+++ src/ddeutil/observe/app.py
@@ -1,9 +1,18 @@
 import uvicorn
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    # Update with specific origins in production
+    allow_origins=["localhost"],
+    allow_methods=["GET", "POST"],
+)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
